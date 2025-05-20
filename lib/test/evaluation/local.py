4a95b56494f17e70889a58dfb3523b66e33823d5
Nolan Wen <wenhn666@163.com>
2025-05-20 16:01:21 +0800

    uppdate

--- lib/test/evaluation/local.py
+++ lib/test/evaluation/local.py
@@ -20,7 +20,8 @@ def local_env_settings():
     settings.prj_dir = '/root/TrackingMamba'
     settings.result_plot_path = '/home/admz/projects/pythonprojects/TrackingMamba/output/test/result_plots'
     settings.results_path = '/home/admz/projects/pythonprojects/TrackingMamba/output/test/tracking_results'    # Where to store tracking results
-    settings.save_dir = '/home/admz/projects/pythonprojects/TrackingMamba/output'
+    #settings.save_dir = '/home/admz/projects/pythonprojects/TrackingMamba/output'
+    settings.save_dir = '/root/TrackingMamba/output'
     settings.segmentation_path = '/home/admz/projects/pythonprojects/TrackingMamba/output/test/segmentation_results'
     settings.tc128_path = '/home/admz/projects/pythonprojects/TrackingMamba/data/TC128'
     settings.tn_packed_results_path = ''
